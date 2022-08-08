00f2ab959e4894582d674ec9687e6ebce7c2c523
praditia07 <110508766+praditia07@users.noreply.github.com>
2022-08-07 18:54:52 -0700

    Add files via upload

---
+++ python1.py
@@ -0,0 +1,12 @@
+# Belajar output
+
+rint("hello world")
+print("nama saya praditia")
+print("hobby bermain game")
+print("saya tinggal di haji icang")
+print("saya lahir di jakarta")
+print("saya suka minta duit")
+print("saya tinggal di Depok")
+print("saya suka berbagi")
+print("saya suka makan")
+print("saya tidak pelit")
