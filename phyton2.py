112ef08d48baf68922fa7759f486a08a28c034da
praditia07 <110508766+praditia07@users.noreply.github.com>
2022-08-07 19:01:58 -0700

    Add files via upload

---
+++ phyton2.py
@@ -0,0 +1,11 @@
+python2.py
+
+nama = "praditia" = praditia adalah data, nama  adalah variabel
+umur = 16
+alamat = "Cimanggis, Depok"
+
+print(nama)
+print(umur)
+print(alamat)
+
+print("nama saya adalah " + nama) 
