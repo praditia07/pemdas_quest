224fa7dd42bec9deff3e61cdf8cbbe1a66e2711b
praditia07 <110508766+praditia07@users.noreply.github.com>
2022-08-11 20:51:03 -0700

    Add files via upload

---
+++ uprak_pradhitia nurdwi a_ x tkj 1.py
@@ -0,0 +1,9 @@
+Nama = input("masukkan nama depan anda: ")
+nama = input("masukkan nama belakang anda: ")
+kelas= input("masukkan kelas anda: ")
+print("nama anda adalah", Nama, nama, "dan anda kelas", kelas)
+
+
+suhu = int(input("masukkan suhu celcius: "))
+reamur = (4/5 * suhu)
+print("suhu, celcius =" , reamur, "reamur")
